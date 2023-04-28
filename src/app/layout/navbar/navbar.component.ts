2408d4145826713c8cca0a54b41a2de2fa36907b
albertorodzba <alberto_rdzbautista@hotmail.com>
2023-04-28 02:13:48 -0600

    [CHORE]: navbar style added (WIP)

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,24 +1,48 @@
-import { Component, HostListener } from '@angular/core';
+
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit{
   menuVisible: boolean = false;
   screenSize: number = window.innerWidth;
+
+  @ViewChild('dropdown') dropdown!: ElementRef;
+  @ViewChild('dropdownContainer') container!: ElementRef<HTMLDivElement>;
+  @ViewChild('navbar') navbar!: ElementRef<HTMLDivElement>;
+  
+  ngAfterViewInit(): void {
+    if(this.screenSize < 769) this.addToDropdownContainer();
+  }
+  
   /**Show the menu when the screen size < 768px */
-  showMenu(){
+  showMenu() {
     this.menuVisible = !this.menuVisible;
     console.log(this.menuVisible);
   }
-
+/**listen to the size of window and executed instructions needed */
   @HostListener('window:resize', ['$event'])
-  onResize():void{
+  onResize(): void {
     this.screenSize = window.innerWidth;
-    if(this.screenSize > 768) this.menuVisible = false;
+    /**reset the change made by addToDropdownContainer() */
+    if (this.screenSize > 768) {
+      this.menuVisible = false; 
+      this.navbar.nativeElement.appendChild(this.dropdown.nativeElement);
+    }
+    if(this.screenSize < 769)
+    {
+      this.addToDropdownContainer()
+    }
+  }
+
+  /**Add the <ul.dropdown> as a child of <div.dropdownContainer> 
+   * to avoid problems on navbar */
+  addToDropdownContainer(){
+    const dropdownContainer = this.container.nativeElement;
+    const dropdownOptions = this.dropdown.nativeElement;
+    dropdownContainer.appendChild(dropdownOptions);
   }
 }
-
-
